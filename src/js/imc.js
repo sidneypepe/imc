a24557f09c832f8ac9161a7cdecc75d28379fe50
sidney <sidneypepe60@gmail.com>
2024-03-27 15:19:16 -0300

    retirada comentario

--- src/js/imc.js
+++ src/js/imc.js
@@ -2,8 +2,7 @@ const form = document.getElementById('form');
 
 
 form.addEventListener('submit', function(event) {
-    /* Previne o comportamento padrão do evento submit do JS, ou seja,
-    impede o recarregamento da página */
+
     event.preventDefault();
 
     const weight = document.getElementById('weight').value;
